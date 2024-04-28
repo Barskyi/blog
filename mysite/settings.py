f1c3f647a4698520c43d880925c95e936c9aff33
Barskyi <barskyi69@gmail.com>
2024-04-28 14:29:24 +0300

    add some code

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,12 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+# STATIC_URL = 'templates/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/templates/'
+STATIC_ROOT = BASE_DIR / 'templates'
